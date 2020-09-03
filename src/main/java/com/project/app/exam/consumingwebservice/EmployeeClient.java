08a69e688a7b3b30cb04ef51ce1aac32438314fc
Harold Negrete <negreteharold@gmail.com>
2020-09-03 17:50:41 -0500

    change URI
    se cambia el localhost por
    https://exam-parameta-soap.herokuapp.com

--- src/main/java/com/project/app/exam/consumingwebservice/EmployeeClient.java
+++ src/main/java/com/project/app/exam/consumingwebservice/EmployeeClient.java
@@ -1,5 +1,6 @@
 package com.project.app.exam.consumingwebservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
 import org.springframework.ws.soap.client.core.SoapActionCallback;
 
@@ -9,13 +10,15 @@ import com.project.app.exam.consumingwebservice.wsdl.SaveEmployeeResponse;
 
 public class EmployeeClient extends WebServiceGatewaySupport {
 
+	@Value("${config.endpoint}")
+	private String endpoint;
+
 	public String saveEmployeeResponse(Employee employee) throws Exception{
 		SaveEmployeeRequest request = new SaveEmployeeRequest();
 		request.setEmployee(employee);
 		SaveEmployeeResponse response = (SaveEmployeeResponse) getWebServiceTemplate()
-				.marshalSendAndReceive("http://localhost:8081/ws/employee", request,
-			            new SoapActionCallback(
-			                    "http://negreteharold.com/exam/parameta/GetCountryRequest"));
+				.marshalSendAndReceive(endpoint+"/ws/employee", request,
+			            new SoapActionCallback("http://negreteharold.com/exam/parameta/GetCountryRequest"));
 		return response.getResponse();
 	}
 
